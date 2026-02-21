96ff9e6c0e21733c64bc6cfcc6e9a710c544fb4d
ayuwidyaagata <ayuwagata@gmail.com>
2026-02-21 23:30:07 +0700

    feat: finalize resume feedback page

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -1,5 +1,8 @@
 import { useEffect, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router"
+import ATS from "~/components/ATS";
+import Details from "~/components/Details";
+import Summary from "~/components/Summary";
 import { usePuterStore } from "~/lib/puter";
 
 export const meta = () => ([
@@ -12,7 +15,7 @@ const resume = () => {
   const { auth, isLoading, fs, kv } = usePuterStore();
   const [imageUrl, setImageUrl] = useState("");
   const [resumeUrl, setResumeUrl] = useState("");
-  const [feedback, setFeedback] = useState("");
+  const [feedback, setFeedback] = useState<Feedback | null>(null);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -67,7 +70,9 @@ const resume = () => {
           <h2 className="text-4xl text-black! font-bold">Resume Review</h2>
           {feedback ? (
             <div className="flex flex-col gap-8 animate-in fade-in duration-1000">
-              Summary ATS Details
+              <Summary feedback={feedback} />
+              <ATS score={feedback.ATS.score || 0} suggestions={feedback.ATS.tips || []} />
+              <Details feedback={feedback} />
             </div>
           ) : (
             <img src="/images/resume-scan-3.gif" alt="no-feedback" className="w-full" />
